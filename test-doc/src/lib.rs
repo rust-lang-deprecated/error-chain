c01ab5aa1b6b7d4577799f42a85906bae2b6b1bc
Yamakaky <yamakaky@yamaworld.fr>
2016-11-14 19:06:07 -0500

    Add test crate to check if all the generated items have an associated doc

---
+++ test-doc/src/lib.rs
@@ -0,0 +1,22 @@
+#[macro_use]
+extern crate error_chain;
+
+/// This module is used to check that all generated items are documented.
+#[deny(missing_docs)]
+pub mod doc {
+    pub mod inner {
+        error_chain! {
+        }
+    }
+
+    error_chain! {
+        links {
+            inner::Error, Test;
+        }
+        errors {
+            Test2 {
+
+            }
+        }
+    }
+}
