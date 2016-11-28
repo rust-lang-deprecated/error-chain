3fab1142846535abb644167a443d1021f307f459
Yamakaky <yamakaky@yamaworld.fr>
2016-11-27 23:48:17 -0500

    [WIP] Define Error struct in root crate.
    Waiting for trait specialization.

--- src/error_chain.rs
+++ src/error_chain.rs
@@ -57,135 +57,56 @@ macro_rules! error_chain_processed {
         /// - an `ErrorKind` which is used to determine the type of the error.
         /// - a backtrace, generated when the error is created.
         /// - an error chain, used for the implementation of `Error::cause()`.
-        #[derive(Debug)]
-        pub struct $error_name {
-            // The members must be `pub` for `links`.
-            /// The kind of the error.
-            #[doc(hidden)]
-            pub kind: $error_kind_name,
-            /// Contains the error chain and the backtrace.
-            #[doc(hidden)]
-            pub state: $crate::State,
-        }
+        pub type $error_name = $crate::Error<$error_kind_name>;
 
-        impl $crate::ChainedError for $error_name {
-            type ErrorKind = $error_kind_name;
+        //$(
+        //    $(#[$meta_links])*
+        //    impl From<$link_error_path> for $error_name {
+        //        fn from(e: $link_error_path) -> Self {
+        //            $error_name {
+        //                kind: $error_kind_name::$link_variant(e.kind),
+        //                state: e.state,
+        //            }
+        //        }
+        //    }
+        //) *
 
-            fn new(kind: $error_kind_name, state: $crate::State) -> $error_name {
-                $error_name {
-                    kind: kind,
-                    state: state,
-                }
-            }
+        //$(
+        //    $(#[$meta_foreign_links])*
+        //    impl From<$foreign_link_error_path> for $error_name {
+        //        fn from(e: $foreign_link_error_path) -> Self {
+        //            $error_name::from_kind(
+        //                $error_kind_name::$foreign_link_variant(e)
+        //            )
+        //        }
+        //    }
+        //) *
 
-            impl_extract_backtrace!($error_name
-                                    $error_kind_name
-                                    $([$link_error_path, $(#[$meta_links])*])*);
-        }
+        //impl From<$error_kind_name> for $error_name {
+        //    fn from(e: $error_kind_name) -> Self {
+        //        $error_name::from_kind(e)
+        //    }
+        //}
 
-        #[allow(dead_code)]
-        impl $error_name {
-            /// Constructs an error from a kind, and generates a backtrace.
-            pub fn from_kind(kind: $error_kind_name) -> $error_name {
-                $error_name {
-                    kind: kind,
-                    state: $crate::State::default(),
-                }
-            }
+        //impl<'a> From<&'a str> for $error_name {
+        //    fn from(s: &'a str) -> Self {
+        //        $error_name::from_kind(s.into())
+        //    }
+        //}
 
-            /// Returns the kind of the error.
-            pub fn kind(&self) -> &$error_kind_name {
-                &self.kind
-            }
+        //impl From<String> for $error_name {
+        //    fn from(s: String) -> Self {
+        //        $error_name::from_kind(s.into())
+        //    }
+        //}
 
-            /// Iterates over the error chain.
-            pub fn iter(&self) -> $crate::ErrorChainIter {
-                $crate::ErrorChainIter(Some(self))
-            }
+        //impl ::std::ops::Deref for $error_name {
+        //    type Target = $error_kind_name;
 
-            /// Returns the backtrace associated with this error.
-            pub fn backtrace(&self) -> Option<&$crate::Backtrace> {
-                self.state.backtrace()
-            }
-        }
-
-        impl ::std::error::Error for $error_name {
-            fn description(&self) -> &str {
-                self.kind.description()
-            }
-
-            fn cause(&self) -> Option<&::std::error::Error> {
-                match self.state.next_error {
-                    Some(ref c) => Some(&**c),
-                    None => {
-                        match self.kind {
-                            $(
-                                $(#[$meta_foreign_links])*
-                                $error_kind_name::$foreign_link_variant(ref foreign_err) => {
-                                    foreign_err.cause()
-                                }
-                            ) *
-                            _ => None
-                        }
-                    }
-                }
-            }
-        }
-
-        impl ::std::fmt::Display for $error_name {
-            fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
-                ::std::fmt::Display::fmt(&self.kind, f)
-            }
-        }
-
-        $(
-            $(#[$meta_links])*
-            impl From<$link_error_path> for $error_name {
-                fn from(e: $link_error_path) -> Self {
-                    $error_name {
-                        kind: $error_kind_name::$link_variant(e.kind),
-                        state: e.state,
-                    }
-                }
-            }
-        ) *
-
-        $(
-            $(#[$meta_foreign_links])*
-            impl From<$foreign_link_error_path> for $error_name {
-                fn from(e: $foreign_link_error_path) -> Self {
-                    $error_name::from_kind(
-                        $error_kind_name::$foreign_link_variant(e)
-                    )
-                }
-            }
-        ) *
-
-        impl From<$error_kind_name> for $error_name {
-            fn from(e: $error_kind_name) -> Self {
-                $error_name::from_kind(e)
-            }
-        }
-
-        impl<'a> From<&'a str> for $error_name {
-            fn from(s: &'a str) -> Self {
-                $error_name::from_kind(s.into())
-            }
-        }
-
-        impl From<String> for $error_name {
-            fn from(s: String) -> Self {
-                $error_name::from_kind(s.into())
-            }
-        }
-
-        impl ::std::ops::Deref for $error_name {
-            type Target = $error_kind_name;
-
-            fn deref(&self) -> &Self::Target {
-                &self.kind
-            }
-        }
+        //    fn deref(&self) -> &Self::Target {
+        //        &self.kind
+        //    }
+        //}
 
 
         // The ErrorKind type
@@ -222,6 +143,12 @@ macro_rules! error_chain_processed {
             }
         }
 
+        impl $crate::ErrorKind for $error_kind_name {
+            fn description(&self) -> &str {
+                self.description()
+            }
+        }
+
         $(
             $(#[$meta_links])*
             impl From<<$link_error_path as $crate::ChainedError>::ErrorKind> for $error_kind_name {
@@ -240,12 +167,6 @@ macro_rules! error_chain_processed {
         impl From<String> for $error_kind_name {
             fn from(s: String) -> Self {
                 $error_kind_name::Msg(s)
-            }
-        }
-
-        impl From<$error_name> for $error_kind_name {
-            fn from(e: $error_name) -> Self {
-                e.kind
             }
         }
     };
