cd599473af1abc8ccb770e2f9a41e3047c6a7b58
Brian Anderson <banderson@mozilla.com>
2016-11-30 00:13:10 -0800

    Polish the quickstart.rs file

--- examples/quickstart.rs
+++ examples/quickstart.rs
@@ -1,41 +1,52 @@
-// This macro uses recursion a lot.
+// Simple and robust error handling with error-chain!
+// Use this as a template for new projects.
+
+// `error_chain!` can recurse deeply
 #![recursion_limit = "1024"]
 
-// Import of the macro. Don't forget to add `error-chain = "*"` in your
+// Import the macro. Don't forget to add `error-chain` in your
 // `Cargo.toml`!
 #[macro_use]
 extern crate error_chain;
 
-// Generations of the `Error` type, the `ErrorKind` enum and the `Result`
-// wrapper. See the documentation for more details.
-//
-// You can also generate the types in a dedicated modules, like `errors.rs`.
-error_chain! {
-    foreign_links {
-        // An IO error can occur.
-        Io(::std::io::Error);
+// We'll put our errors in an `errors` module, and other modules in
+// this crate will `use errors::*;` to get access to everything
+// `error_chain!` creates.
+mod errors {
+    // Create the Error, ErrorKind, ResultExt, and Result types
+    error_chain! { }
+}
+
+use errors::*;
+
+fn main() {
+    if let Err(ref e) = run() {
+        println!("error: {}", e);
+
+        for e in e.iter().skip(1) {
+            println!("caused by: {}", e);
+        }
+
+        // The backtrace is not always generated. Try to run this example
+        // with `RUST_BACKTRACE=1`.
+        if let Some(backtrace) = e.backtrace() {
+            println!("backtrace: {:?}", backtrace);
+        }
+
+        ::std::process::exit(1);
     }
 }
 
-// With the `Result` wrapper, we don't have to specify `Error` each time.
-fn do_some_work() -> Result<()> {
+// Most functions will return the `Result` type, imported from the
+// `errors` module. It is a typedef of the standard `Result` type
+// for which the error type is always our own `Error`.
+fn run() -> Result<()> {
     use std::fs::File;
-    // Dummy file, this operation should fail on your system. The result of
-    // `File::open` is automatically converted to our `Error` type.
-    File::open("tretrete")?;
+
+    // This operation will fail
+    File::open("tretrete")
+        .chain_err(|| "unable to open tretrete file")?;
+
     Ok(())
 }
 
-fn main() {
-    match do_some_work() {
-        Ok(()) => println!("No errors"),
-        Err(e) => {
-            println!("An error occured: {}", e);
-            // The backtrace is not always generated. Try to run this example
-            // with `RUST_BACKTRACE=1`.
-            if let Some(backtrace) = e.backtrace() {
-                println!("Backtrace: {:?}", backtrace);
-            }
-        }
-    }
-}
