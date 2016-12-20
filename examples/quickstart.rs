cca3cf9cd57ab7240505fcc44ada0580b83efc21
test9753 <test9753@mailinator.com>
2016-12-20 09:01:55 +0530

    Print error messages to stderr in quickstart and quick_main.

--- examples/quickstart.rs
+++ examples/quickstart.rs
@@ -21,16 +21,20 @@ use errors::*;
 
 fn main() {
     if let Err(ref e) = run() {
-        println!("error: {}", e);
+        use ::std::io::Write;
+        let stderr = &mut ::std::io::stderr();
+        let errmsg = "Error writing to stderr";
+
+        writeln!(stderr, "error: {}", e).expect(errmsg);
 
         for e in e.iter().skip(1) {
-            println!("caused by: {}", e);
+            writeln!(stderr, "caused by: {}", e).expect(errmsg);
         }
 
         // The backtrace is not always generated. Try to run this example
         // with `RUST_BACKTRACE=1`.
         if let Some(backtrace) = e.backtrace() {
-            println!("backtrace: {:?}", backtrace);
+            writeln!(stderr, "backtrace: {:?}", backtrace).expect(errmsg);
         }
 
         ::std::process::exit(1);
