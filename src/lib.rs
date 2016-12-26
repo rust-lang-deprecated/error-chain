b3398c342eeb58a456a0632bad8c5525977c6c19
Nelson Chen <crazysim@gmail.com>
2016-12-26 12:05:11 -0800

    Call it section, like the rest of the documentation section.

--- src/lib.rs
+++ src/lib.rs
@@ -107,7 +107,8 @@
 //! error_chain! {
 //!     // The type defined for this error. These are the conventional
 //!     // and recommended names, but they can be arbitrarily chosen.
-//!     // It is also possible to leave this block out entirely, or
+//!     //
+//!     // It is also possible to leave this section out entirely, or
 //!     // leave it empty, and these names will be used automatically.
 //!     types {
 //!         Error, ErrorKind, ResultExt, Result;
