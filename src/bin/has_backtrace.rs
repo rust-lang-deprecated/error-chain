69d6a00e6fbb8b75169fd093e468a8871e7b42c8
Linus Färnstrand <faern@faern.net>
2017-08-29 11:43:28 +0200

    Add standalone binary for backtrace tests

---
+++ src/bin/has_backtrace.rs
@@ -0,0 +1,17 @@
+//! Exits with exit code 0 if backtraces are disabled and 1 if they are enabled.
+//! Used by tests to make sure backtraces are available when they should be.
+
+#[macro_use]
+extern crate error_chain;
+
+error_chain! {
+    errors {
+        MyError
+    }
+}
+
+fn main() {
+    let err = Error::from(ErrorKind::MyError);
+    let has_backtrace = err.backtrace().is_some();
+    ::std::process::exit(has_backtrace as i32);
+}
