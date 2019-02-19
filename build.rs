865d56f7055a3794e02eca23e60120c3cf9795c2
Tom Parker-Shemilt <palfrey@tevp.net>
2019-02-19 22:25:34 +0000

    Fix delimiter in build.rs

--- build.rs
+++ build.rs
@@ -5,7 +5,7 @@ use version_check::{is_min_date, is_min_version, is_nightly};
 fn main() {
     // Switch on for versions that have Error::source
     // As introduced by https://github.com/rust-lang/rust/pull/53533
-    if ((is_nightly().unwrap_or(false)
+    if (is_nightly().unwrap_or(false)
             && is_min_date("2018-09-02")
                 .unwrap_or((false, "".to_string())).0)
         || is_min_version("1.30").unwrap_or((false, "".to_string())).0
