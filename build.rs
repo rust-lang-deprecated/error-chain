c78ced288f0c502ff807c06b6f8ceb797da70977
Tom Parker-Shemilt <palfrey@tevp.net>
2019-03-09 16:25:54 +0000

    Strip down supported versions to "at least 1.30"

--- build.rs
+++ build.rs
@@ -1,14 +1,11 @@
 extern crate version_check;
 
-use version_check::{is_min_date, is_min_version, is_nightly};
+use version_check::is_min_version;
 
 fn main() {
     // Switch on for versions that have Error::source
     // As introduced by https://github.com/rust-lang/rust/pull/53533
-    if (is_nightly().unwrap_or(false)
-            && is_min_date("2018-09-02")
-                .unwrap_or((false, "".to_string())).0)
-        || is_min_version("1.30").unwrap_or((false, "".to_string())).0
+    if is_min_version("1.30").map(|(is_high_enough, _actual_version)| is_high_enough).unwrap_or(false)
     {
         println!("cargo:rustc-cfg=has_error_source");
     }
